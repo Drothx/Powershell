58d1d3685a1ac1946465d55d95ba5f2760a29915
Drothx <64139713+Drothx@users.noreply.github.com>
2022-05-11 00:17:04 +0200

    Create script3.ps1

---
+++ script3.ps1
@@ -0,0 +1,23 @@
+# Le but étant de créer un script qui prend une chaine de caractère donné par l’utilisateur et qui l’affiche mot par mot. 
+#
+# EX: «Nous sommes au cours de Scripting»
+# ******************************************************
+# *                                                    *
+#               Nous sommes au cours de scripting       
+# *               *****************************        *
+# *                         Nom Host                   *
+# *                    Répertoire courant              *
+# *                     ***************                *
+# ******************************************************
+# 
+# 
+
+
+Write-Host "******************************************************"
+Write-Host "*                                                    *"
+Write-Host "            "$args
+Write-Host "*               *****************************        *"
+Write-Host "                       "$env:COMPUTERNAME
+Write-Host "            "$Pwd
+Write-Host "*                     ***************                *"
+Write-Host "******************************************************"
